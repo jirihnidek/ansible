700dbb98511da7b6665aac971aba2bc15335df81
Jiri Hnidek <jhnidek@redhat.com>
2019-06-28 14:42:32 +0200

    WIP: added first unit test of redhat_subscriptio module.

---
+++ test/units/modules/packaging/os/test_redhat_subscription.py
@@ -0,0 +1,59 @@
+# (c) 2018, Sean Myers <sean.myers@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from units.compat.mock import call, patch
+from ansible.modules.packaging.os import redhat_subscription
+
+from units.modules.utils import (AnsibleExitJson, ModuleTestCase)
+
+
+class RedHatSubscriptionModuleTestCase(ModuleTestCase):
+    module = rhsm_release
+
+    def setUp(self):
+        super(RedHatSubscriptionModuleTestCase, self).setUp()
+
+        # Mainly interested that the subscription-manager calls are right
+        # based on the module args, so patch out run_command in the module.
+        # returns (rc, out, err) structure
+        self.mock_run_command = patch('ansible.modules.packaging.os.rhsm_release.'
+                                      'AnsibleModule.run_command')
+        self.module_main_command = self.mock_run_command.start()
+
+        # Module does a get_bin_path check before every run_command call
+        self.mock_get_bin_path = patch('ansible.modules.packaging.os.rhsm_release.'
+                                       'AnsibleModule.get_bin_path')
+        self.get_bin_path = self.mock_get_bin_path.start()
+        self.get_bin_path.return_value = '/testbin/subscription-manager'
+
+    def tearDown(self):
+        self.mock_run_command.stop()
+        self.mock_get_bin_path.stop()
+        super(RedHatSubscriptionModuleTestCase, self).tearDown()
+
+    def module_main(self, exit_exc):
+        with self.assertRaises(exit_exc) as exc:
+            self.module.main()
+        return exc.exception.args[0]
+
+    def test_register_username_password_org(self):
+        """
+        Test registration using username, admin and org without auto-attach
+        """
+        set_module_args(
+        	{
+        	'state': 'present'
+        	'username': 'admin',
+        	'password': 'admin',
+        	'org_id': 'admin'
+        	})
+
+        result = self.module_main(AnsibleExitJson)
+
+        self.assertTrue(result['changed'])
+        self.module_main_command.assert_has_calls([
+            call('/testbin/subscription-manager register --username admin --password admin --org admin', check_rc=True),
+        ])
